cf75ba7efb061241bdd865072f170949cd66f37a
Yurii Yelisieiev <w1ckaero@gmail.com>
2019-06-08 11:24:23 +0300

    Fixed slider and added normal webpack config

--- src/app/slider.js
+++ src/app/slider.js
@@ -1,39 +1,59 @@
-function start(){
+(function start() {
     let carousel = document.querySelector('.Slider');
     carouselize(carousel);
-}
+})();
 
-function carouselize(carousel){
+function carouselize(carousel) {
     let productList = carousel.querySelector('.Product_wrapper__list');
     let productListWidth = 0;
-    let productListSteps = 0;
     let products = carousel.querySelectorAll('.Slider__product');
     let productAmount = 0;
-    let productAmountVisible = 3;
+    let to_show = 5;
     let carouselPrev = carousel.querySelector('.prev_button');
     let carouselNext = carousel.querySelector('.next_button');
-    [].forEach.call(products, function(product){
+    let direction = -1;
+    [].forEach.call(products, function (product) {
         productAmount++;
-        productListWidth += 150;
-        productList.style.width = productListWidth+"px";
+        if (product.className === "Product_wrapper_available") {
+            productListWidth += 150;
+            productList.style.width = productListWidth + "px";
+        }
     });
 
-    function moveProductList() {
-        productList.style.transform = "translateX(-"+150*productListSteps+"px)";
+    function show_first_elements() {
+        for (let i = 0; i < productList.children.length; i++) {
+            if (i < to_show) {
+                productList.children[i].className = "Product_wrapper_available"
+            } else {
+                productList.children[i].className = "Product_wrapper_hidden"
+            }
+        }
     }
 
-    carouselNext.onclick = function() {
-        if(productListSteps < productAmount-productAmountVisible) {
-            productListSteps++;
-            moveProductList();
+    carousel.addEventListener('transitionend', function () {
+        if (direction === -1) {
+            productList.firstElementChild.className = 'Product_wrapper_hidden';
+            productList.appendChild(productList.firstElementChild);
+        } else if (direction === 1) {
+            productList.lastElementChild.className = 'Product_wrapper_hidden';
+            productList.prepend(productList.lastElementChild);
         }
-    };
+        show_first_elements();
+        productList.style.transition = 'none';
+        productList.style.transform = "translateX(0)";
+        setTimeout(function () {
+            productList.style.transition = 'all 0.5s';
+        });
+    });
 
-    carouselPrev.onclick = function() {
-        if(productListSteps > 0) {
-            productListSteps--;
-            moveProductList();
-        }
-    };
+    carouselNext.addEventListener('click', function () {
+        direction = -1;
+        productList.style.transform = "translateX(-20%)";
+    });
+
+
+    carouselPrev.addEventListener('click', function () {
+        direction = 1;
+        productList.style.transform = "translateX(20%)";
+    })
 }
-start();
